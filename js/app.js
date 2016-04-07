654ba17545b35966534a7328d3220fdcc3e05b35
daniellelara <daniellelaragourgey@gmail.com>
2016-04-07 19:21:59 +0100

    I feel stupid

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('dose',  ['ngResource', 'satellizer', 'angular-jwt', 'gridster', 'ngFileUpload'])
-  .constant('API_URL', 'https://wdi-dose-api')
+  .constant('API_URL', 'https://wdi-dose-api.herokuapp.com')
   .constant('YT', 'https://www.googleapis.com/youtube/v3/search')
-  .constant('API', 'https://wdi-dose-api/users')
+  .constant('API', 'https://wdi-dose-api.herokuapp.com/users')
   .constant('S3', 'https://s3-eu-west-1.amazonaws.com/wdi-london18/')
   .config(oauthConfig);
 
