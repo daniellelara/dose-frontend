8b0ad0e561d349163e47e74e321efedab0538fda
daniellelara <daniellelaragourgey@gmail.com>
2016-04-26 11:45:20 +0100

    changed word of the day to https

--- js/services/word.js
+++ js/services/word.js
@@ -8,7 +8,7 @@ function Word($http) {
   return {
     get: function() {
       
-        return $http.get('http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+        return $http.get('https://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
       }
     }
   }
